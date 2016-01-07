2ad879dd2aa4f702752dbcf4c35be0cb4a8c6ee1
johngueringer <gueringerjohn@gmail.com>
2016-01-07 10:26:49 -0600

    fixed typos/updated sensor class

--- sensor.py
+++ sensor.py
@@ -4,30 +4,25 @@ import datetime as dt
 
 
 class Sensor(object):
-    def __init__(self, code, node):
+    def __init__(self, code):
         self.code = code
-        self.node = node
-        self._data = list()
+        self._data = []
         self._sensor_name = None
-        self._timestamp = None
-        self._units = None
+        self._dtype = []
         self._context = None
 
     @property
     def data(self):
         return self._data
 
-    @property
-    def timestamp(self):
-        return self._timestamp
 
     @property
     def sensor_name(self):
         return self._sensor_name
 
     @property
-    def units(self):
-        return self._units
+    def dtype(self):
+        return self._dtype
 
     @property
     def context(self):
@@ -37,43 +32,88 @@ class Sensor(object):
         arr = line.split(',')
         name = arr[0]
         timestamp = dt.datetime.strptime(arr[1], '%m/%d/%y %H:%M:%S')
-        units = []
-        self._timestamp = timestamp
+        dtypes = []
+        self._sensor_name = name
 
         data = arr[2:]
         for datum in data:
             info = datum.split(';')
-            val = float(info[1])
-            units.append(info[2])
+            type = info[0]
+            unit = info[2]
+            val = (timestamp, float(info[1]))
             context = info[3]
-            self._data = val
-            self._units = units
+            
+            dtype = (type, unit)
+            dtypes.append(dtype)
+            
+            self._data.append(val)
+            self._dtype = dtypes
             self._context = context
-
-
-
-
-class GridSensor(Sensor):
-    def parse(self, line):
-
+    
+class DualSensor(Sensor):
+    def __init__(self, code):
+        super(DualSensor, self).__init__(code)
+        self._data2 = []
+    
+    @property
+    def data2(self):
+        return data2
+    
+    @property
+    def add_point(self, line):
         arr = line.split(',')
         name = arr[0]
         timestamp = dt.datetime.strptime(arr[1], '%m/%d/%y %H:%M:%S')
-        sensor = {'sensorname': name,
-                  'timestamp': timestamp}
-        temps = []
-        units = []
-        pixels = []
-        for content in arr[2:]:
-            data = content.split(";")
-            key = data[0]
-            val = float(data[1])
-            temps.append(val)
-            units.append(data[2])
-            pixels.append(data[3])
+        dtypes = []
+        self._sensor_name = name
 
-            sensor[key] = temps
-            sensor['units'] = units
-            sensor['context'] = pixels
+        data = arr[2:]
+        i = 0
+        for datum in data:
+            info = datum.split(';')
+            type = info[0]
+            unit = info[2]
+            val = (timestamp, float(info[1]))
+            context = info[3]
+            
+            dtype = (type, unit)
+            dtypes.append(dtype)
+            
+            self._dtype = dtypes
+            self._context = context
+            if i == 0:
+                self._data.append(val)
+            else:
+                self._data2.append(val)
+            i+=1
+            
+class GridSensor(Sensor):
+    def __init__(self, code):
+        super(GridSensor, self).__init__(code)
+        
+    def add_point(self, line):
+        arr = line.split(',')
+        name = arr[0]
+        timestamp = dt.datetime.strptime(arr[1], '%m/%d/%y %H:%M:%S')
+        vals = []
+        dtypes = []
+        self._sensor_name = name
 
-        return sensor
+        data = arr[2:]
+        i = 0
+        for datum in data:
+            info = datum.split(';')
+            type = info[0]
+            unit = info[2]
+            val = (timestamp, float(info[1]))
+            context = info[3]
+            
+            dtype = (type, unit)
+            dtypes.append(dtype)
+            vals.append(val)
+        
+            self._dtype = dtypes
+            self._context = context
+            
+        self._data.append(vals)
+            
