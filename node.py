2e4cb728ccb708b6708900717c390f6b4779be30
johngueringer <gueringerjohn@gmail.com>
2016-02-04 04:53:44 -0600

    Documented code and AoT class

--- node.py
+++ node.py
@@ -5,11 +5,22 @@ import datetime as dt
 from ConfigParser import ConfigParser
 from constants import SENSOR_CODES, GRID_SENSOR, DATA_URI
 from sensor import Sensor, GridSensor, DualSensor
+import numpy as np
+import matplotlib as mtplt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cm as cmaps
+import scipy.stats as stats
 
 
 # config = ConfigParser('config.ini')
 
-
+"""
+Node class:
+INSTANCE VARIABLES
+    node     : String      : name of the AoT node
+    sensors  : Dictionary  : Dictionary of Sensor objects
+"""
 class Node(object):
     def __init__(self, node):
         self._node = node
@@ -30,6 +41,21 @@ class Node(object):
     def sensor(self, code):
         return next((v for k, v in self.sensors.iteritems() if k == code), None)
 
+    """
+    The following method pulls the AoT sensor data from the specified start time to 
+    the specified stop time, and stores the data in the Sensor objects of the sensors 
+    dictionary.
+    
+    :param strt : First date from which to start pulling data
+    :type  strt : String
+    
+    :param stp  : Last date from which data will be pulled
+    :type  stp  : String
+    
+    :return     : This method stores the data from wa8.gl in appropriate Sensor objects
+    :rtype      : void
+    
+    """
     def pull_all(self, strt, stp):
         urls = self.makeURLs(strt, stp)
         sensors = {'MLX90614ESF-DAA': Sensor,
@@ -64,8 +90,20 @@ class Node(object):
                 print "Missing data from: " + url
                 
         grid_sensor = self.sensor('D6T-44L-06')
-        grid_sensor.parse()
+        grid_sensor.sort_by_pixel()
 
+    """
+    Generates a list of URLs from which to pull AoT data
+    
+    :param strt_dte : First date from which URLs will be generated
+    :type  strt_dte : String
+    
+    :param stp_dte  : Last date from which URLs will be generated
+    :type  stp_dte  : String
+    
+    :return         : List of URLs from which to pull data
+    :rtype          : Array of Strings
+    """
     def makeURLs(self, strt_dte, stp_dte):
         urls = []
 
@@ -99,6 +137,44 @@ class Node(object):
             strt += delta
 
         return urls
-
+    
+    """
+    Converts fromatted string to datetime object
+    
+    :param s  : Formatted string
+    :type  s  : String
+    :return   : datetime object representing the date
+    :rtype    : datetime object
+    """
     def to_datetime(self, s):
         return dt.datetime.strptime(s, '%m/%d/%y %H:%M:%S')
+    
+    """
+    Plots data contained in all the sensors of the AoT node
+    
+    :param None : None
+    :type  None : void
+    :return     : None
+    :rtype      : void
+    """
+    def plot_all(self):
+        sub_plots = []
+        l = 5
+        w = 3
+        grid = (l, w)
+        for i in range(l):
+            for j in range(w):
+                sub_plot = plt.subplot2grid(grid, (i, j), rowspan=1, colspan=1)
+                sub_plots.append(sub_plot)
+
+        sensors = self._sensors.iteritems()
+        for sub_plot in sub_plots:
+            sensor = sensors.next()[1]
+            if sensor is GridSensor:
+                pass
+            else:
+                sensor.plot_timeseries(sub_plot)
+        
+        plt.show()
+            
+            
--- sensor.py
+++ sensor.py
@@ -9,9 +9,17 @@ import matplotlib as mtplt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.cm as cmaps
-from matplotlib import animation as anim
 import scipy.stats as stats
 
+"""
+Sensor class for encapsulating data in AoT sensors:
+INSTANCE VARIABLES
+    code        :  String  : Model of the AoT sensor
+    data        :  Array   : list of data points (datetime, float) tuples
+    sensor_name :  String  : Full name of sensor
+    dtype       :  Array   : list of types of data (type of data, units) tuples
+    context     :  String  : Additional sensor info
+"""
 class Sensor(object):
     def __init__(self, code):
         self._code = code
@@ -40,6 +48,14 @@ class Sensor(object):
     def context(self):
         return self._context
 
+    """
+    Adds one data point to the Sensor from a formatted string
+    
+    :param line : Formatted line containing data to be added
+    :type  line : String
+    :return     : None
+    :rtype      : void
+    """
     def add_point(self, line):
         arr = line.split(',')
         name = arr[0]
@@ -62,38 +78,87 @@ class Sensor(object):
             self._dtype = dtypes
             self._context = context
     
-    def plot_timeseries(self):
-        data = self._data
-        dtype = self._dtype
-        name = self._sensor_name
+    """
+    Creates a timeseries plot from the data in the Sensor
+    
+    :param None  : None
+    :type  None  : void
+    :return      : Subplot of timeseries
+    :rtype       : matplotlib subplot
+    """
+    def plot_timeseries(self, subplot = None):
+        if subplot is None:
+            data = self._data
+            dtype = self._dtype
+            name = self._sensor_name
 
-        x, y = zip(*data)
-        type, units = zip(*dtype)
-        type = type[0]
-        units = units[0]
+            x, y = zip(*data)
+            type0, units = zip(*dtype)
+            type0 = type0[0]
+            units = units[0]
 
-        n = len(x)
-        strt = x[1].strftime("%m/%d/%y")
-        stp = x[n - 1].strftime("%m/%d/%y")
+            n = len(x)
+            strt = x[1].strftime("%m/%d/%y")
+            stp = x[n - 1].strftime("%m/%d/%y")
 
-        xlab = 'Time of Day'
-        ylab_tmpl = '{} ({})'
-        ylab = ylab_tmpl.format(type, units)
-        title_tmpl = '{}: {}\n{} - {}'
-        title = title_tmpl.format(name, type, strt, stp)
+            xlab = 'Time of Day'
+            ylab_tmpl = '{} ({})'
+            ylab = ylab_tmpl.format(type0, units)
+            title_tmpl = '{}: {}\n{} - {}'
+            title = title_tmpl.format(name, type0, strt, stp)
 
-        fig = plt.figure() 
-        ax1 = plt.subplot()
-        ax1.plot(x, y, 'r-', label=type)
-        handles, labels = ax1.get_legend_handles_labels()
-        ax1.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
-        #ax1.gcf().autofmt_xdate()
-        ax1.set_xlabel(xlab)
-        ax1.set_ylabel(ylab)
-        ax1.set_title(title)
-        plt.show()
+            fig = plt.figure() 
+            ax1 = plt.subplot()
+            ax1.plot(x, y, 'r-', label=type)
+            handles, labels = ax1.get_legend_handles_labels()
+            ax1.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
+            #ax1.gcf().autofmt_xdate()
+            ax1.set_xlabel(xlab)
+            ax1.set_ylabel(ylab)
+            ax1.set_title(title)
 
+            return ax1
+        else:
+            data = self._data
+            dtype = self._dtype
+            name = self._sensor_name
 
+            x, y = zip(*data)
+            type0, units = zip(*dtype)
+            type0 = type0[0]
+            units = units[0]
+
+            n = len(x)
+            strt = x[1].strftime("%m/%d/%y")
+            stp = x[n - 1].strftime("%m/%d/%y")
+
+            xlab = 'Time of Day'
+            ylab_tmpl = '{} ({})'
+            ylab = ylab_tmpl.format(type0, units)
+            title_tmpl = '{}: {}\n{} - {}'
+            title = title_tmpl.format(name, type0, strt, stp)
+
+            subplot.plot(x, y, 'r-', label=type0)
+            handles, labels = subplot.get_legend_handles_labels()
+            subplot.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
+            subplot.set_xlabel(xlab)
+            subplot.set_ylabel(ylab)
+            subplot.set_title(title)
+            
+    """
+    Creates a timeseries plot from the data in the Sensor
+    
+    :param subplot  : Empty subplot
+    :type  subplot  : matplotlib subplot
+    :return         : None
+    :rtype          : void
+    """ 
+        
+"""
+Dual Sensor subclass for AoT sensors that collect two types of data:
+ADDITIONAL VARIABLES
+    data2       :  Array   : Second list of data points (datetime, float) tuples
+"""
 class DualSensor(Sensor):
     def __init__(self, code):
         super(DualSensor, self).__init__(code)
@@ -103,6 +168,14 @@ class DualSensor(Sensor):
     def data2(self):
         return self._data2
     
+    """
+    Adds one data point to the DualSensor from a formatted string
+    
+    :param line : Formatted line containing data to be added
+    :type  line : String
+    :return     : None
+    :rtype      : void
+    """
     def add_point(self, line):
         arr = line.split(',')
         name = arr[0]
@@ -130,51 +203,118 @@ class DualSensor(Sensor):
                 self._data2.append(val)
             i+=1
     
-    def plot_timeseries(self):
-        data = self._data
-        data2 = self._data2
-        dtype = self._dtype
-        name = self._sensor_name
+    """
+    Creates a timeseries plot from the data in the DualSensor
+    
+    :param None  : None
+    :type  None  : void
+    :return      : Subplot of timeseries
+    :rtype       : matplotlib subplot
+    """
+    def plot_timeseries(self, subplot = None):
+        if subplot is None:
+            data = self._data
+            data2 = self._data2
+            dtype = self._dtype
+            name = self._sensor_name
 
-        x, y1 = zip(*data)
-        y2 = zip(*data2)[1]
-        
-        types, units = zip(*dtype)
-        type1 = types[0]
-        type2 = types[1]
-        unit1 = units[0]
-        unit2 = units[1]
+            x, y1 = zip(*data)
+            y2 = zip(*data2)[1]
 
-        n = len(x)
-        strt = x[1].strftime("%m/%d/%y")
-        stp = x[n - 1].strftime("%m/%d/%y")
+            types, units = zip(*dtype)
+            type1 = types[0]
+            type2 = types[1]
+            unit1 = units[0]
+            unit2 = units[1]
 
-        xlab = 'Time of Day'
-        
-        ylab_tmpl = '{} ({})'
-        y1lab = ylab_tmpl.format(type1, unit1)
-        y2lab = ylab_tmpl.format(type2, unit2)
-        
-        title_tmpl = '{}: {} and {}\n{} - {}'
-        title = title_tmpl.format(name, type1, type2, strt, stp)
-               
-        fig = plt.figure() 
-        ax1 = plt.subplot()
-        ax1.plot(x, y1, 'r-', label=y1lab)
-        handles, labels = ax1.get_legend_handles_labels()
-        ax1.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
-        #ax1.gcf().autofmt_xdate()
-        ax1.set_xlabel(xlab)
-        ax1.set_ylabel(y1lab)
-        ax1.set_title(title)
-        
-        ax2 = ax1.twinx()
-        ax2.plot(x, y2, 'b-', label=y2lab)
-        handles, labels = ax2.get_legend_handles_labels()
-        ax2.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 0.95))
-        ax2.set_ylabel(y2lab)
-        plt.show()
-        
+            n = len(x)
+            strt = x[1].strftime("%m/%d/%y")
+            stp = x[n - 1].strftime("%m/%d/%y")
+
+            xlab = 'Time of Day'
+
+            ylab_tmpl = '{} ({})'
+            y1lab = ylab_tmpl.format(type1, unit1)
+            y2lab = ylab_tmpl.format(type2, unit2)
+
+            title_tmpl = '{}: {} and {}\n{} - {}'
+            title = title_tmpl.format(name, type1, type2, strt, stp)
+
+            fig = plt.figure() 
+            ax1 = plt.subplot()
+            ax1.plot(x, y1, 'r-', label=y1lab)
+            handles, labels = ax1.get_legend_handles_labels()
+            ax1.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
+            ax1.set_xlabel(xlab)
+            ax1.set_ylabel(y1lab)
+            ax1.set_title(title)
+
+            ax2 = ax1.twinx()
+            ax2.plot(x, y2, 'b-', label=y2lab)
+            handles, labels = ax2.get_legend_handles_labels()
+            ax2.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 0.95))
+            ax2.set_ylabel(y2lab)
+
+            return ax1, ax2
+        else:
+            data = self._data
+            data2 = self._data2
+            dtype = self._dtype
+            name = self._sensor_name
+
+            x, y1 = zip(*data)
+            y2 = zip(*data2)[1]
+
+            types, units = zip(*dtype)
+            type1 = types[0]
+            type2 = types[1]
+            unit1 = units[0]
+            unit2 = units[1]
+
+            n = len(x)
+            strt = x[1].strftime("%m/%d/%y")
+            stp = x[n - 1].strftime("%m/%d/%y")
+
+            xlab = 'Time of Day'
+
+            ylab_tmpl = '{} ({})'
+            y1lab = ylab_tmpl.format(type1, unit1)
+            y2lab = ylab_tmpl.format(type2, unit2)
+
+            title_tmpl = '{}: {} and {}\n{} - {}'
+            title = title_tmpl.format(name, type1, type2, strt, stp)
+
+            fig = plt.figure()
+            subplot.plot(x, y1, 'r-', label=y1lab)
+            handles, labels = subplot.get_legend_handles_labels()
+            subplot.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 1))
+            subplot.set_xlabel(xlab)
+            subplot.set_ylabel(y1lab)
+            subplot.set_title(title)
+
+            ax2 = subplot.twinx()
+            ax2.plot(x, y2, 'b-', label=y2lab)
+            handles, labels = ax2.get_legend_handles_labels()
+            ax2.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.95, 0.95))
+            ax2.set_ylabel(y2lab)
+    
+    """
+    Creates a timeseries plot from the data in the DualSensor
+    
+    :param subplot  : Empty subplot
+    :type  None     : matplotlib subplot
+    :return         : None
+    :rtype          : void
+    """
+    
+    """
+    Correlates the two sets of data in the DualSensor and plots the correlation.
+    
+    :param None  : None
+    :type  None  : void
+    :return      : Subplot of correlation
+    :rtype       : matplotlib subplot
+    """
     def plot_correlation(self):
         data = self._data
         data2 = self._data2
@@ -212,12 +352,25 @@ class DualSensor(Sensor):
         ax1.set_xlabel(y1lab)
         ax1.set_ylabel(y2lab)
         ax1.set_title(title)
-        plt.show()
-            
+        
+        return ax1
+
+"""
+Grid Sensor subclass for the AoT IR grid sensor. This class overrides methods from the Sensor class
+to properly work with grid data
+"""
 class GridSensor(Sensor):
     def __init__(self, code):
         super(GridSensor, self).__init__(code)
-        
+    
+    """
+    Adds one data point to the GridSensor from a formatted string
+    
+    :param line : Formatted line containing data to be added
+    :type  line : String
+    :return     : None
+    :rtype      : void
+    """
     def add_point(self, line):
         arr = line.split(',')
         name = arr[0]
@@ -230,12 +383,12 @@ class GridSensor(Sensor):
         i = 0
         for datum in data:
             info = datum.split(';')
-            type = info[0]
+            type0 = info[0]
             unit = info[2]
             val = float(info[1])
             context = info[3]
             
-            dtype = (type, unit)
+            dtype = (type0, unit)
             dtypes.append(dtype)
             vals.append(val)
         
@@ -244,7 +397,15 @@ class GridSensor(Sensor):
             
         self._data.append((timestamp, vals))
           
-    def parse(self):
+    """
+    Sorts the data in the GridSensor by pixel.
+    
+    :param None : None
+    :type  None : void
+    :return     : None
+    :rtype      : void
+    """
+    def sort_by_pixel(self):
         pixels = []
         for i in range(17):
             pix = []
@@ -265,6 +426,15 @@ class GridSensor(Sensor):
 
         self._data = pixels 
     
+    """
+    Creates a timeseries plot for each pixel in the grid
+    
+    :param None  : None
+    :type  None  : void
+    :return      : List of subplots for the pixels in the grid
+    :rtype       : Array
+    
+    """
     def plot_timeseries(self):
         pixs = self._data
         sub_plots = []
@@ -287,4 +457,4 @@ class GridSensor(Sensor):
             plt.grid(True)
             i+=1
 
-        plt.show()
+        return sub_plots
